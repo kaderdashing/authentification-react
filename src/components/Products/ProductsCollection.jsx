09e415468a8fd650b3ba0da6c9b9ee96048d14a9
HichemNgz <neggazhichem@gmail.com>
2024-06-10 13:12:30 +0100

    update

--- src/components/Products/ProductsCollection.jsx
+++ src/components/Products/ProductsCollection.jsx
@@ -1,32 +1,52 @@
-import React from 'react'
-import ProductCard from './ProductCard'
-import { Select, Button } from '@chakra-ui/react';
+import React from 'react';
+import ProductCard from './ProductCard';
+import { Select, Button, Flex } from '@chakra-ui/react';
 import product from '../../assets/product.png';
 import ProductCard2 from './ProductCard2';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import { IconContext } from 'react-icons';
+import { BiLeftArrowCircle, BiRightArrowCircle } from 'react-icons/bi';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { useSearchParams } from 'react-router-dom';
 
-const ProductsCollection = () => {
-  const [products, setProducts] = useState(null)
+const ProductsCollection = ({ result }) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = searchParams.get('page');
 
-  useEffect(() => {
-    const getProducts = async () => {
-      await axios.get()
-    }
-  }, [])
+  const handlePageClick = (event) => {
+    const updatedSearchParams = new URLSearchParams(searchParams);
+    updatedSearchParams.set('page', event.selected + 1);
+    setSearchParams(updatedSearchParams.toString());
+  };
   return (
-    <div className='grid w-full grid-cols-1 gap-5 lg:grid-cols-2 xl:gap-10'>
-      <ProductCard2/>
-      
-      <ProductCard2/>
-      <ProductCard2/>
-      <ProductCard2/>
-      <ProductCard2/>
-      <ProductCard2/>
-      
-    </div>
-  )
-}
+    <>
+      <div className="grid w-full grid-cols-1 gap-5 lg:grid-cols-2 xl:gap-10">
+        {result?.data?.map((product, index) => (
+          <ProductCard2 key={index} product={product} />
+        ))}
+      </div>
 
-export default ProductsCollection
+      <div className="w-full flex items-center justify-center mt-6">
+        <ReactPaginate
+          breakLabel="..."
+          previousLabel={Number(currentPage) === 1 ? null : <ChevronLeft size={24} />}
+          nextLabel={
+            Number(currentPage) === result?.meta?.total ? null : <ChevronRight size={24} />
+          }
+          pageClassName={'page-item'}
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          pageCount={result?.meta?.total}
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          activeClassName="active"
+          previousLinkClassName="previous"
+          nextLinkClassName="next"
+          disabledClassName="disabled"
+        />
+      </div>
+    </>
+  );
+};
+
+export default ProductsCollection;
