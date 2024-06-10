0c2bac2bc89f68d0ea2735ee0c308d27a654412a
HichemNgz <neggazhichem@gmail.com>
2024-06-10 13:11:34 +0100

    update

--- src/components/Products/ProductsCollection.jsx
+++ src/components/Products/ProductsCollection.jsx
@@ -3,8 +3,18 @@ import ProductCard from './ProductCard'
 import { Select, Button } from '@chakra-ui/react';
 import product from '../../assets/product.png';
 import ProductCard2 from './ProductCard2';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const ProductsCollection = () => {
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      await axios.get()
+    }
+  }, [])
   return (
     <div className='grid w-full grid-cols-1 gap-5 lg:grid-cols-2 xl:gap-10'>
       <ProductCard2/>
